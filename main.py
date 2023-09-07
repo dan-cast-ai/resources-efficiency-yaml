9428f8d4e187aacff5570294c56872150fb7f797
dan-cast-ai <144298427+dan-cast-ai@users.noreply.github.com>
2023-09-07 10:09:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from constants import CLUSTERS_URL, CLUSTER_NAME, WORKLOADS_URL_POST, EXTERNAL_CLUSTERS_URL, \
     COST_REPORTS_URL_PRE, WORKLOAD_EFFICIENCY_URL_START_POST, EFFICIENCY_URL_END_POST, \
-    EFFICIENCY_URL_START_POST, GB_BYTES
+    EFFICIENCY_URL_START_POST, GB_BYTES, API_KEY
 
 from datetime import datetime, timedelta
 import json
@@ -8,10 +8,14 @@ import requests
 import yaml
 import os
 
+
 def get_list(url):
+    api_key = os.getenv('CAST_AI_API_KEY')
+    if not api_key:
+        api_key = API_KEY
     headers = {
         "accept": "application/json",
-        "X-API-Key": f"{os.getenv('CAST_AI_API_KEY')}"
+        "X-API-Key": api_key
     }
 
     try:
@@ -61,8 +65,7 @@ def find_cluster_id(clusters, name: str) -> str:
 
 
 def create_yaml(workload_efficiency_list, start, end):
-    resources = []
-    with open(f'resources_efficiency_{datetime.now().strftime("%Y%m%d%H%M%S")}.yaml', 'w') as file:
+    with open(f'resources_efficiency_{datetime.now().strftime("%Y%m%d%H%M%S")}.yaml', 'w') as manifest_file:
         for item in workload_efficiency_list:
             url = (f"{COST_REPORTS_URL_PRE}{cluster_id}/namespaces/{item['namespace']}/"
                    f"{item['workloadType']}/{item['workloadName']}/{EFFICIENCY_URL_START_POST}{start}"
@@ -73,26 +76,30 @@ def create_yaml(workload_efficiency_list, start, end):
             resource = {
                 "containers": containers
             }
-            file.write(f"--- {item['workloadName']} --- {item['workloadType']} --- {item['namespace']} ---\n")
+
             for container_raw_data in resources_raw_data['containers']:
-                requests = []
-                container = {
-                    "name": container_raw_data['name'],
-                    "requests": requests
-                }
                 if len(container_raw_data['items']):
-                    request = {
-                        "memory": convert_gb_to_bytes(container_raw_data['items'][0]['info']['recommendation']['memoryGib']),
+                    memory = f"{container_raw_data['items'][0]['info']['recommendation']['memoryGib']}Gi"
+                    requests = {
+                        "memory": memory,
                         "cpu": convert_to_cpu_request(container_raw_data['items'][0]['info']['recommendation']['cpu'])
                     }
-                    requests.append(request)
+                    limits = {
+                        "memory": memory
+                    }
+                    container = {
+                        "name": container_raw_data['name'],
+                        "requests": requests,
+                        "limits": limits
+                    }
                 containers.append(container)
-            resources.append(resource)
+            if len(containers):
+                manifest_file.write(f"--- {item['workloadName']} (Workload Name) "
+                                    f"--- {item['workloadType']} (Workload Type)"
+                                    f"--- {item['namespace']} (Namespace) ---\n\n")
 
-        # print("")
-    manifest = {"resources": resources}
-    # print("")
-    return yaml.dump(manifest)
+                yaml.dump(resource, manifest_file, sort_keys=False)
+                manifest_file.write("\n")
 
 
 def convert_gb_to_bytes(gb):
@@ -127,7 +134,4 @@ if __name__ == '__main__':
     start_time = seven_days_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
     end_time = current_time_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
     workload_efficiency_list = get_workloads_efficiency(cluster_id, start_time, end_time)
-    yaml = create_yaml(workload_efficiency_list, start_time, end_time)
-    print("")
-    with open(f'resources_efficiency_{datetime.now().strftime("%Y%m%d%H%M%S")}.yaml', 'w') as file:
-        file.write(yaml)
+    create_yaml(workload_efficiency_list, start_time, end_time)
